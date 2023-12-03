596b0878020b774bcd82d4b2d94597ae7957d539
UmairSaeed <123866496+Sadness7311@users.noreply.github.com>
2023-12-04 00:08:50 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <div className="nav">
             <i className="ri-menu-fill" onClick={() => gsap.to('.menu', { height: '100vh' })}></i>
-            <h3>Discord</h3>
+            <h3>Social</h3>
             <h3>Bots</h3>
         </div>
     )
